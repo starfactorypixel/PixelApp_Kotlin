021908d27807114f447e2584855a8d0966d531e9
Vladislav Sumin <vladislavsumin@mail.ru>
2022-08-03 00:44:54 +0300

    Add AutoSizeText

---
+++ client/android/src/main/java/ru/starfactory/pixel/ui/view/AutoSizeText.kt
@@ -0,0 +1,37 @@
+package ru.starfactory.pixel.ui.view
+
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun AutoSizeText(
+    modifier: Modifier = Modifier,
+    text: String,
+    textStyle: TextStyle = LocalTextStyle.current,
+) {
+    var scaledTextStyle by remember { mutableStateOf(textStyle) }
+    var readyToDraw by remember { mutableStateOf(false) }
+
+    Text(
+        text,
+        modifier.drawWithContent {
+            if (readyToDraw) {
+                drawContent()
+            }
+        },
+        style = scaledTextStyle,
+        softWrap = false,
+        onTextLayout = { textLayoutResult ->
+            if (textLayoutResult.didOverflowWidth || textLayoutResult.didOverflowHeight) {
+                scaledTextStyle =
+                    scaledTextStyle.copy(fontSize = scaledTextStyle.fontSize * 0.9)
+            } else {
+                readyToDraw = true
+            }
+        }
+    )
+}

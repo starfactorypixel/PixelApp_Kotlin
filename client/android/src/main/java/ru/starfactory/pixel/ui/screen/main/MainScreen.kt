0cdaf26d5d939b64fb6eb62fe0f1d7f1cd2e4c78
Vladislav Sumin <vladislavsumin@mail.ru>
2022-08-03 18:37:01 +0300

    Add navigation drawer to main screen

--- client/android/src/main/java/ru/starfactory/pixel/ui/screen/main/MainScreen.kt
+++ client/android/src/main/java/ru/starfactory/pixel/ui/screen/main/MainScreen.kt
@@ -1,9 +1,35 @@
 package ru.starfactory.pixel.ui.screen.main
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import ru.starfactory.pixel.ui.screen.dashboard.DashboardScreen
+import ru.starfactory.pixel.ui.screen.main_menu.MainMenuScreen
 
 @Composable
 fun MainScreen() {
-    DashboardScreen()
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        drawerContent = { MainMenuScreen() }
+    ) {
+        Box(Modifier.padding(it)) {
+            IconButton(
+                onClick = { scope.launch { scaffoldState.drawerState.open() } },
+                modifier = Modifier.padding(4.dp),
+            ) {
+                Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+            }
+            DashboardScreen()
+        }
+    }
 }
---
+++ client/android/src/main/java/ru/starfactory/pixel/ui/screen/main_menu/MainMenuScreen.kt
@@ -0,0 +1,8 @@
+package ru.starfactory.pixel.ui.screen.main_menu
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainMenuScreen() {
+
+}

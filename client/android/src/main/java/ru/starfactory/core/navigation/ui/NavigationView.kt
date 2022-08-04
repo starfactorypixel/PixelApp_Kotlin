cc589e01c1dd68192ca32b3d681b4872edc326e8
Vladislav Sumin <vladislavsumin@mail.ru>
2022-08-04 23:30:44 +0300

    add screen animation

--- client/android/src/main/java/ru/starfactory/core/navigation/ui/NavigationView.kt
+++ client/android/src/main/java/ru/starfactory/core/navigation/ui/NavigationView.kt
@@ -1,7 +1,9 @@
 package ru.starfactory.core.navigation.ui
 
 import androidx.compose.runtime.Composable
+import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.jetpack.stack.Children
+import com.arkivanov.decompose.extensions.compose.jetpack.stack.animation.*
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.value.Value
 import ru.starfactory.core.navigation.Screen
@@ -9,8 +11,14 @@ import ru.starfactory.core.navigation.Screen
 @Composable
 fun NavigationContentView(childStack: Value<ChildStack<Screen, ScreenInstance>>) {
     Children(
-        childStack
+        childStack,
+        animation = animation()
     ) { childCreated ->
         childCreated.instance.ScreenInstanceView()
     }
 }
+
+@OptIn(ExperimentalDecomposeApi::class)
+@Composable
+private fun animation(): StackAnimation<Screen, ScreenInstance> =
+    stackAnimation { _, _ -> scale() + fade() }

9b4d20d9779a22adca63f8debed1460177dca010
Vladislav Sumin <vladislavsumin@mail.ru>
2022-08-05 23:15:23 +0300

    add no devices view content

--- client/android/src/main/java/ru/starfactory/pixel/ui/screen/debug/usb/UsbView.kt
+++ client/android/src/main/java/ru/starfactory/pixel/ui/screen/debug/usb/UsbView.kt
@@ -1,8 +1,13 @@
 package ru.starfactory.pixel.ui.screen.debug.usb
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import ru.starfactory.core.decompose.view_model.decomposeViewModel
 
 @Composable
@@ -22,7 +27,9 @@ private fun UsbContent(state: UsbViewState) {
 
 @Composable
 private fun UsbNoDevices() {
-
+    Box(Modifier.fillMaxSize()) {
+        Text(text = "No Devices", Modifier.align(Alignment.Center))
+    }
 }
 
 @Composable

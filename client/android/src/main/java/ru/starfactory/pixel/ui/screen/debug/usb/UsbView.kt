cad7dc518fdba71d9d76c7155dba72f2aff26b74
Vladislav Sumin <vladislavsumin@mail.ru>
2022-08-05 23:30:31 +0300

    add has usb devices view content

--- client/android/src/main/java/ru/starfactory/pixel/ui/screen/debug/usb/UsbView.kt
+++ client/android/src/main/java/ru/starfactory/pixel/ui/screen/debug/usb/UsbView.kt
@@ -1,13 +1,17 @@
 package ru.starfactory.pixel.ui.screen.debug.usb
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import ru.starfactory.core.decompose.view_model.decomposeViewModel
 
 @Composable
@@ -34,5 +38,27 @@ private fun UsbNoDevices() {
 
 @Composable
 fun UsbHasDevices(devices: List<UsbViewState.UsbDeviceState>) {
-
+    Divider()
+    LazyColumn {
+        items(devices, key = { it.name }) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(text = it.name, style = MaterialTheme.typography.h6)
+                Row {
+                    Text(text = "Vendor", Modifier.weight(1f))
+                    Text(text = "ID:${it.vendorId}", Modifier.weight(1f))
+                    Text(text = "Name:${it.vendorName}", Modifier.weight(2f))
+                }
+                Row {
+                    Text(text = "Product", Modifier.weight(1f))
+                    Text(text = "ID:${it.productId}", Modifier.weight(1f))
+                    Text(text = "Name:${it.productName}", Modifier.weight(2f))
+                }
+            }
+            Divider()
+        }
+    }
 }

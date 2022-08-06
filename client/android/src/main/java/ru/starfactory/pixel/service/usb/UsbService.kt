5f0f5b63f5076944ed8498e321756b1c56cfd251
Vladislav Sumin <vladislavsumin@mail.ru>
2022-08-06 14:08:51 +0300

    UsbService add requestPermission for usb device functional

--- client/android/src/main/java/ru/starfactory/pixel/service/usb/UsbService.kt
+++ client/android/src/main/java/ru/starfactory/pixel/service/usb/UsbService.kt
@@ -1,10 +1,15 @@
 package ru.starfactory.pixel.service.usb
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.IntentFilter
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import ru.starfactory.core.utils.observeBroadcastIntents
@@ -13,6 +18,7 @@ import ru.starfactory.pixel.Tag
 interface UsbService {
     fun observeUsbDevices(): Flow<Map<String, UsbDevice>>
     fun getUsbDevices(): Map<String, UsbDevice>
+    suspend fun requestPermission(device: UsbDevice): Boolean
 }
 
 class UsbServiceImpl(private val context: Context) : UsbService {
@@ -35,7 +41,29 @@ class UsbServiceImpl(private val context: Context) : UsbService {
         return usbManager.deviceList
     }
 
+    override suspend fun requestPermission(device: UsbDevice): Boolean {
+        if (usbManager.hasPermission(device)) return true
+
+        return coroutineScope {
+            val requestPermissionIntent =
+                PendingIntent.getBroadcast(context, 0, Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE)
+            val filter = IntentFilter(ACTION_USB_PERMISSION)
+
+            val deferredResponse = async {
+                context.observeBroadcastIntents(filter, false)
+                    .first()
+            }
+
+            usbManager.requestPermission(device, requestPermissionIntent)
+
+            val response = deferredResponse.await()
+
+            response.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)
+        }
+    }
+
     companion object {
         private const val TAG = Tag.USB
+        private const val ACTION_USB_PERMISSION = "com.starfactory.pixel.USB_PERMISSION"
     }
 }

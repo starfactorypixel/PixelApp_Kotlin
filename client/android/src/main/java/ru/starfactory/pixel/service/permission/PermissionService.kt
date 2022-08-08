07e0c71f399faa60b36fa8612eaaa1e4863d5715
Vladislav Sumin <vladislavsumin@mail.ru>
2022-08-08 21:39:01 +0300

    update PermissionService.kt

--- client/android/src/main/java/ru/starfactory/pixel/service/permission/PermissionService.kt
+++ client/android/src/main/java/ru/starfactory/pixel/service/permission/PermissionService.kt
@@ -4,16 +4,16 @@ import android.content.Context
 import android.content.pm.PackageManager
 
 interface PermissionService {
-    suspend fun requestPermission(permission: String): Boolean
+    suspend fun requestPermission(permission: Permission): Boolean
 }
 
 class PermissionServiceImpl(private val context: Context) : PermissionService {
     var permissionRequester: PermissionRequester? = null
 
-    override suspend fun requestPermission(permission: String): Boolean {
-        if (context.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) return true
+    override suspend fun requestPermission(permission: Permission): Boolean {
+        if (context.checkSelfPermission(permission.raw) == PackageManager.PERMISSION_GRANTED) return true
 
-        return permissionRequester?.requestPermission(permission = permission)
+        return permissionRequester?.requestPermission(permission = permission.raw)
             ?: throw RuntimeException("Permission requester not attached")
     }
 }

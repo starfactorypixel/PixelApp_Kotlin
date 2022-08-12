3eb8f044a8d4311c9bc503f8e3cb0cb9d4fefce1
Vladislav Sumin <vladislavsumin@mail.ru>
2022-08-12 21:37:18 +0300

    add :core:compose module

---
+++ core/decompose/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("ru.starfactory.convention.preset.core")
+    id("org.jetbrains.compose")
+}
+
+kotlin {
+    sourceSets {
+        named("commonMain") {
+            dependencies {
+                api(libs.decompose.core)
+                api(libs.decompose.jetbrains)
+
+                api(libs.kodein.compose)
+
+                implementation(project(":core:compose"))
+            }
+        }
+    }
+}

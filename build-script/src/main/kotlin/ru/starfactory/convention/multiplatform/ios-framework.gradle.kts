42a1593d83408cc1df1f2ebc57b0e7bcb58821be
Vladislav Sumin <vladislavsumin@mail.ru>
2022-08-14 12:57:20 +0300

    move ios framework settings to new multiplatform convention

---
+++ build-script/src/main/kotlin/ru/starfactory/convention/multiplatform/ios-framework.gradle.kts
@@ -0,0 +1,49 @@
+package ru.starfactory.convention.multiplatform
+
+plugins {
+    id("kotlin-multiplatform")
+}
+
+kotlin {
+    iosX64("uikitX64") {
+        binaries {
+            framework {
+                freeCompilerArgs += listOf(
+                    "-linker-option", "-framework", "-linker-option", "Metal",
+                    "-linker-option", "-framework", "-linker-option", "CoreText",
+                    "-linker-option", "-framework", "-linker-option", "CoreGraphics"
+                )
+            }
+        }
+    }
+    iosArm64("uikitArm64") {
+        binaries {
+            framework {
+                freeCompilerArgs += listOf(
+                    "-linker-option", "-framework", "-linker-option", "Metal",
+                    "-linker-option", "-framework", "-linker-option", "CoreText",
+                    "-linker-option", "-framework", "-linker-option", "CoreGraphics"
+                )
+                // TODO: the current compose binary surprises LLVM, so disable checks for now.
+                freeCompilerArgs += "-Xdisable-phases=VerifyBitcode"
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+
+        val iosMain by creating {
+            dependsOn(commonMain)
+        }
+
+        val uikitMain by creating {
+            dependsOn(iosMain)
+        }
+        val uikitX64Main by getting {
+            dependsOn(uikitMain)
+        }
+        val uikitArm64Main by getting {
+            dependsOn(uikitMain)
+        }
+    }
+}

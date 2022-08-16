50f82ccb7cdc731b2f0857ec440627236071de7e
Vladislav Sumin <vladislavsumin@mail.ru>
2022-08-16 17:44:44 +0300

    Create vertical menu

--- feature/main-screen/src/commonMain/kotlin/ru/starfactory/pixel/main_screen/ui/screen/main/MainView.kt
+++ feature/main-screen/src/commonMain/kotlin/ru/starfactory/pixel/main_screen/ui/screen/main/MainView.kt
@@ -1,8 +1,50 @@
 package ru.starfactory.pixel.main_screen.ui.screen.main
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Apps
+import androidx.compose.material.icons.filled.BatteryChargingFull
+import androidx.compose.material.icons.filled.DirectionsCar
+import androidx.compose.material.icons.filled.Navigation
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import ru.starfactory.pixel.main_screen.ui.widged.main_menu.PVerticalMainMenu
+import ru.starfactory.pixel.main_screen.ui.widged.main_menu.PVerticalMenuItem
 
 @Composable
 fun MainView() {
+    MainContent()
+}
 
+@Composable
+private fun MainContent() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            //TODO Sumin - вынести цвета в тему
+            .background(Brush.linearGradient(colors = listOf(Color(0xFF435159), Color(0xFF1F292E))))
+    ) {
+        var selectedItemIndex by remember { mutableStateOf(1) }
+        Row(modifier = Modifier.fillMaxHeight()) {
+            PVerticalMainMenu(
+                items = listOf(
+                    PVerticalMenuItem(Icons.Default.DirectionsCar, "General"),
+                    PVerticalMenuItem(Icons.Default.Navigation, "Navigation"),
+                    PVerticalMenuItem(Icons.Default.Apps, "Apps"),
+                    PVerticalMenuItem(Icons.Default.BatteryChargingFull, "Charging"),
+                ),
+                Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(16.dp),
+                selectedItemIndex = selectedItemIndex,
+                onClickItem = { selectedItemIndex = it },
+                isShowTitle = true
+            )
+        }
+    }
 }
---
+++ feature/main-screen/src/commonMain/kotlin/ru/starfactory/pixel/main_screen/ui/widged/main_menu/PVerticalMainMenu.kt
@@ -0,0 +1,93 @@
+package ru.starfactory.pixel.main_screen.ui.widged.main_menu
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+internal fun PVerticalMainMenu(
+    items: List<PVerticalMenuItem>,
+    modifier: Modifier = Modifier,
+    selectedItemIndex: Int = -1,
+    onClickItem: (Int) -> Unit = {},
+    isShowTitle: Boolean = true,
+) {
+    Row(modifier.height(IntrinsicSize.Max)) {
+        // Column with icons
+        Column(
+            Modifier
+                .clip(RoundedCornerShape(percent = 100))
+                .background(Color(0xFF1A2227)) //TODO sumin вынести в тему
+        ) {
+            items.forEachIndexed { index, menuItem ->
+                ItemIconContent(menuItem.icon, index == selectedItemIndex, onClickItem = { onClickItem(index) })
+            }
+        }
+
+        // Column with text
+        if (isShowTitle) {
+            Column(
+                Modifier
+                    .fillMaxHeight()
+                    .width(IntrinsicSize.Max)
+            ) {
+                items.forEachIndexed { index, menuItem ->
+                    ItemDescriptionContent(menuItem.text, index == selectedItemIndex, onClickItem = { onClickItem(index) })
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ItemIconContent(icon: ImageVector, isSelected: Boolean, onClickItem: () -> Unit) {
+    val targetColor = if (isSelected) Color(0xFF07AFD4) else Color.Transparent
+    val backgroundColor by animateColorAsState(targetColor)
+    Icon(
+        icon, null,
+        Modifier
+            .background(backgroundColor)
+            .clickable { onClickItem() }
+            .padding(8.dp)
+            .size(24.dp)
+    )
+}
+
+@Composable
+private fun ColumnScope.ItemDescriptionContent(text: String, isSelected: Boolean, onClickItem: () -> Unit) {
+    val targetColor = if (isSelected) Color(0xFF07AFD4) else MaterialTheme.colors.onSurface
+    val textColor by animateColorAsState(targetColor)
+    val interactionSource = remember { MutableInteractionSource() }
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .weight(1f)
+            .clickable(interactionSource, indication = null) { onClickItem() }
+    ) {
+        Text(
+            text,
+            Modifier
+                .align(Alignment.CenterVertically)
+                .padding(horizontal = 16.dp),
+            color = textColor
+        )
+    }
+}
+
+
+internal data class PVerticalMenuItem(val icon: ImageVector, val text: String)

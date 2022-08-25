52e3280d70f1401513fcbcf3bb098099c6fec731
Vladislav Sumin <vladislavsumin@mail.ru>
2022-08-25 20:04:06 +0300

    update select source view

--- feature/ecu-connection/src/commonMain/kotlin/ru/starfactory/pixel/ecu_connection/ui/screen/select_source/SelectSourceView.kt
+++ feature/ecu-connection/src/commonMain/kotlin/ru/starfactory/pixel/ecu_connection/ui/screen/select_source/SelectSourceView.kt
@@ -1,20 +1,24 @@
 package ru.starfactory.pixel.ecu_connection.ui.screen.select_source
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.ScrollableDefaults
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Usb
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import ru.starfactory.core.compose.paddingSystemWindowInsets
+import ru.starfactory.core.uikit.layout.FlexVerticalGrid
+import ru.starfactory.core.uikit.view.POutlinedCard
 import ru.starfactory.pixel.ecu_connection.domain.source.Source
-import ru.starfactory.pixel.ecu_connection.domain.source.SourceType
 
 @Composable
 internal fun SelectSourceView() {
@@ -24,51 +28,92 @@ internal fun SelectSourceView() {
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun SelectSourceContent() {
-    val sources: Map<SourceType, List<Source>> = mapOf(
-        SourceType.DEMO to listOf(Source.Demo)
+    val sources: List<Source> = listOf(
+        Source.Demo,
+        Source.Demo,
+        Source.Demo,
+        Source.Demo,
+        Source.Demo,
+        Source.Demo,
+        Source.Demo,
+        Source.Demo,
+        Source.Demo,
+        Source.Demo,
+        Source.Demo,
+        Source.Demo,
     )
 
-    LazyColumn(
+    val overscrollEffect = ScrollableDefaults.overscrollEffect()
+
+    Box(
         Modifier
             .fillMaxSize()
-            .paddingSystemWindowInsets(),
+            .paddingSystemWindowInsets()
     ) {
-        sources.forEach { (sourcesType, sources) ->
-            stickyHeader(key = sourcesType) {
-                SourceHeader(sourcesType)
+        Column(
+            Modifier
+                .align(Alignment.Center)
+                .verticalScroll(rememberScrollState())
+        ) {
+            FlexVerticalGrid(
+                3,
+                Modifier
+                    .padding(16.dp)
+                    .overscroll(overscrollEffect)
+                    .scrollable(
+                        rememberScrollableState { 0f },
+                        Orientation.Vertical,
+                        overscrollEffect
+                    ),
+                verticalSpacing = 16.dp,
+                horizontalSpacing = 16.dp
+            ) {
+                sources.forEach {
+                    SourceContent(it)
+                }
             }
-            items(sources) { source ->
-                SourceContent(source)
-            }
+        }
+
+    }
+}
+
+@Composable
+private fun SourceContent(source: Source) {
+    POutlinedCard(
+        Modifier
+            .defaultMinSize(minWidth = 180.dp, minHeight = 100.dp)
+            .clickable { }
+    ) {
+        when (source) {
+            Source.Demo -> DefaultSourceContent(Icons.Default.Usb, "Arduino Uno")
         }
     }
 }
 
 @Composable
-private fun SourceHeader(sourceType: SourceType) {
-    Text(
-        sourceType.name,
+private fun DefaultSourceContent(
+    icon: ImageVector,
+    text: String
+) {
+    Box(
         Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.primary.copy(alpha = 0.15f))
             .padding(16.dp, 8.dp)
-    )
-}
-
-@Composable
-private fun SourceContent(source: Source) {
-    return when (source) {
-        Source.Demo -> DemoSourceContent()
+    ) {
+        Row(
+            Modifier
+                .align(Alignment.Center)
+        ) {
+            Icon(
+                icon,
+                null,
+                Modifier
+                    .align(Alignment.CenterVertically)
+            )
+            Text(
+                text,
+                Modifier
+                    .align(Alignment.CenterVertically)
+            )
+        }
     }
-}
-
-@Composable
-private fun DemoSourceContent() {
-    Text(
-        "Demo source",
-        Modifier
-            .fillMaxSize()
-            .clickable { }
-            .padding(16.dp, 8.dp)
-    )
 }

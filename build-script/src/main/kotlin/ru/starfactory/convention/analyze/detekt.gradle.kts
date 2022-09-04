f17bc5c4c010b0f959aeb84bf94900a214b77f14
Vladislav Sumin <vladislavsumin@mail.ru>
2022-09-04 20:59:31 +0300

    add detekt convention

---
+++ build-script/src/main/kotlin/ru/starfactory/convention/analyze/detekt.gradle.kts
@@ -0,0 +1,34 @@
+package ru.starfactory.convention.analyze
+
+import io.gitlab.arturbosch.detekt.Detekt
+import org.gradle.accessors.dm.LibrariesForLibs
+
+plugins {
+    id("io.gitlab.arturbosch.detekt")
+}
+
+val libs = the<LibrariesForLibs>()
+
+val detektConfigFile = rootProject.projectDir.resolve("detektConfig").resolve("detekt.yml")
+
+// Конфигурирем на уровне тасок, а не на уровне плагина, так как таски созданные в ручную
+// не подтягивают дефолтные значения из конфигурации плагина
+tasks.withType<Detekt>().configureEach {
+    autoCorrect = true
+    parallel = true
+    buildUponDefaultConfig = true
+    config.setFrom(files(detektConfigFile))
+}
+
+// Исправляем путь к файлам только для дефолтных detekt тасок
+tasks.named<Detekt>("detekt").configure {
+    source = fileTree(project.projectDir) {
+        include("src/**/*")
+        include("build.gradle.kts")
+        include("settings.gradle.kts")
+    }
+}
+
+dependencies {
+    detektPlugins(libs.detekt.formatting)
+}

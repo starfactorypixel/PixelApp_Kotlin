7629291e193ff0acb7d97547e1c729562c196eea
Vladislav Sumin <vladislavsumin@mail.ru>
2022-09-08 22:55:50 +0300

    add try carch to git exec

--- build-script/src/main/kotlin/ru/starfactory/build_script/git/Git.kt
+++ build-script/src/main/kotlin/ru/starfactory/build_script/git/Git.kt
@@ -1,19 +1,25 @@
 package ru.starfactory.build_script.git
 
 import org.gradle.api.Project
+import org.gradle.process.internal.ExecException
 import java.io.ByteArrayOutputStream
 
 fun Project.gitGetLatestVersionTag(): String {
     val data = ByteArrayOutputStream()
     val error = ByteArrayOutputStream()
-    val result = exec {
-        executable = "git"
-        args = listOf("describe", "--tags", "--match", "v*")
-        standardOutput = data
-        errorOutput = error
-    }
-    check(result.exitValue == 0) {
-        "git return non zero value: ${result.exitValue}, data='$data', error='$error'"
+    try {
+        val result = exec {
+            executable = "git"
+            args = listOf("describe", "--tags", "--match", "v*")
+            standardOutput = data
+            errorOutput = error
+        }
+        check(result.exitValue == 0) {
+            "git return non zero value: ${result.exitValue}, data='$data', error='$error'"
+        }
+    } catch (e: ExecException) {
+        logger.error("exec failed, data='$data', error='$error'")
+        throw e
     }
     return data.toString()
 }

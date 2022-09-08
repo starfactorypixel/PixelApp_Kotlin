b7603803875f132e56639cd346b8e051fa3a0ebc
Vladislav Sumin <vladislavsumin@mail.ru>
2022-09-08 19:00:32 +0300

    add release signature && assemble release apk

--- build-script/src/main/kotlin/ru/starfactory/convention/android/signature.gradle.kts
+++ build-script/src/main/kotlin/ru/starfactory/convention/android/signature.gradle.kts
@@ -17,11 +17,27 @@ android {
             keyAlias = "pixel-android"
             keyPassword = "pixel_app"
         }
+
+        if (useReleaseKeys) {
+            create("release") {
+                val password = System.getenv("RELEASE_KEYSTORE_PASSWORD")
+                check(password != null) { "environment variable RELEASE_KEYSTORE_PASSWORD not found" }
+
+                storeFile = rootProject.projectDir.resolve("keys").resolve("release.jks")
+                storePassword = password
+
+                keyAlias = "pixel-android"
+                keyPassword = password
+            }
+        }
     }
 
     buildTypes {
         debug {
             signingConfig = signingConfigs.getByName("debug")
+        }
+        release {
+            signingConfig = signingConfigs.getByName(if (useReleaseKeys) "release" else "debug")
         }
     }
 }

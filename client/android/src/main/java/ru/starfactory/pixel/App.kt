d8c6510e58f52633fd17b9e7fc68f60fc5f5d909
Vladislav Sumin <vladislavsumin@mail.ru>
2022-09-09 20:22:03 +0300

    add dispatcher for application scope

--- client/android/src/main/java/ru/starfactory/pixel/App.kt
+++ client/android/src/main/java/ru/starfactory/pixel/App.kt
@@ -3,6 +3,7 @@ package ru.starfactory.pixel
 import android.app.Application
 import android.content.Context
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import org.kodein.di.DI
 import org.kodein.di.DIAware
@@ -10,7 +11,7 @@ import org.kodein.di.bindSingleton
 import ru.starfactory.core.di.Modules
 
 class App : Application(), DIAware {
-    private val applicationScope = CoroutineScope(SupervisorJob())
+    private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
     override val di: DI by DI.lazy {
         bindSingleton<Context> { this@App }
         bindSingleton { applicationScope }

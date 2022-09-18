53c1a3cd3976551c2d3dc926118b0d11c1b7f73e
Vladislav Sumin <vladislavsumin@mail.ru>
2022-09-18 15:59:49 +0300

    add keep-screen-on feature module

---
+++ feature/keep-screen-on/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("ru.starfactory.convention.preset.client-feature")
+    id("org.jetbrains.compose")
+    id("kotlin-parcelize") // TODO
+}
+
+kotlin {
+    sourceSets {
+        named("commonMain") {
+            dependencies {
+            }
+        }
+    }
+}
